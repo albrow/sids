6ac4b71496208cff31bcc3c2fa4d65f7cb65b507
stephenalexbrowne <stephenalexbrowne@gmail.com>
2013-03-02 23:06:41 -0500

    Add bracket_controller and CRUD stubs for the bracket views

--- app/controllers/brackets_controller.rb
+++ app/controllers/brackets_controller.rb
@@ -1,17 +1,29 @@
 class BracketsController < ApplicationController
 
+def index
+	# list all the brackets for the current user
+end
+
 def new
-	# will return all the teams in the first round formatted as json
+	# render a new bracket page
 end
 
 def create
 	# will create a new bracket for the user
 end
 
+def show
+	# show a particular bracket
+end
+
+def edit
+	# render an edit page
+end
+
 def update
 	# will update a users bracket
 end
 
-def delete
-
+def destroy
+	# delete the bracket (remove it from database)
 end
