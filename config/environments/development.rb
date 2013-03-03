6b25ecad15ca27452d4ecf7ee6e1549d96f8bf9c
stephenalexbrowne <stephenalexbrowne@gmail.com>
2013-03-02 20:08:33 -0500

    Add devise gem. Setup basic devise. Add placeholder for homepage.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ Sids::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # Development email server for Devise
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
