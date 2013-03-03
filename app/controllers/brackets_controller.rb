8ebb73fbb4d7e90a21fae0b10e5f77c6e57378f3
stephenalexbrowne <stephenalexbrowne@gmail.com>
2013-03-02 22:50:13 -0500

    Add a brackets controller

---
+++ app/controllers/brackets_controller.rb
@@ -0,0 +1,17 @@
+class BracketsController < ApplicationController
+
+def new
+	# will return all the teams in the first round formatted as json
+end
+
+def create
+	# will create a new bracket for the user
+end
+
+def update
+	# will update a users bracket
+end
+
+def delete
+
+end
