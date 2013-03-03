b9c74aa785446d2d2d16a0605c4161cb5603c78e
fabioberger <fabioberger1991@gmail.com>
2013-03-02 20:39:24 -0500

    Added template

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,12 @@ Sids::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
+      # Add the fonts path
+  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+
+  # Precompile additional assets
+  config.assets.precompile += %w( .svg .eot .woff .ttf )
+
   # Development email server for Devise
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
