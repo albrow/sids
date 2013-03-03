ba020f19569b45ce6bad0edeb7f6a043faa118fe
fabioberger <fabioberger1991@gmail.com>
2013-03-03 00:05:02 -0500

    added choice page temp

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Sids::Application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   match '/about' => 'home#about'
+  match '/choice' => 'home#choice'
   match '/getgames' => 'game#getGames'
   resources :photos
 
