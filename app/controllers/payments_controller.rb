344344d5e9f8591a6251000f29e7a2bb82222339
fabioberger <fabioberger1991@gmail.com>
2013-03-07 14:04:45 -0500

    added donations to payments

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -7,7 +7,11 @@ class PaymentsController < ApplicationController
 
 	def create
 	  # Amount in cents
-	  @amount = 200
+	  @amt = params[:payment_amt]
+
+	  if @amt.match(/\A[+-]?\d+?(\.\d+)?\Z/) == nil
+	  	@amt = 200
+	  end
 
 	  customer = Stripe::Customer.create(
 	    :email => current_user.email,
@@ -16,14 +20,14 @@ class PaymentsController < ApplicationController
 
 	  charge = Stripe::Charge.create(
 	    :customer    => customer.id,
-	    :amount      => @amount,
+	    :amount      => @amt,
 	    :description => 'Bracket Customer',
 	    :currency    => 'usd'
 	  )
 
 	rescue Stripe::CardError => e
 	  flash[:error] = e.message
-	  redirect_to charges_path
+	  redirect_to :action => "new"
 	end
 
 end
