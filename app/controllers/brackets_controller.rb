a07290afedcc097713d475709f898be7c68e64a9
stephenalexbrowne <stephenalexbrowne@gmail.com>
2013-03-08 01:09:18 -0500

    Reorganized bracket show page. Sass now uses changable variables and a lot of nifty (yet fucking difficult) math.

--- app/controllers/brackets_controller.rb
+++ app/controllers/brackets_controller.rb
@@ -64,6 +64,7 @@ class BracketsController < ApplicationController
 			:east => @east,
 			:midwest => @midwest
 		}
+
 		respond_to do |format|
 	    format.html  # index.html.erb
 	    format.json  { render :json => @bracket }
