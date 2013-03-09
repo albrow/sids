d5661bd6e68f1a6607a6eca96f7893d490c06588
stephenalexbrowne <stephenalexbrowne@gmail.com>
2013-03-08 23:52:50 -0500

    Fix migration add_activation_to_bracket to use default => false instead of default => 0. PG didn't like the old value

--- db/migrate/20130308063942_add_activation_to_bracket.rb
+++ db/migrate/20130308063942_add_activation_to_bracket.rb
@@ -1,5 +1,5 @@
 class AddActivationToBracket < ActiveRecord::Migration
   def change
-    add_column :brackets, :active, :boolean, default: 0
+    add_column :brackets, :active, :boolean, default: false
   end
 end
