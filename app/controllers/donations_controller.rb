c4fe7f63cb5a7e054caa25c8b5f1b890056941b4
fabioberger <fabioberger1991@gmail.com>
2013-03-22 01:26:48 -0400

    Added donation button and func to homepage

---
+++ app/controllers/donations_controller.rb
@@ -0,0 +1,35 @@
+class DonationsController < ApplicationController
+
+	def new
+	end
+
+	def create
+	  # Amount in cents
+	  @amt = params[:payment_amt]
+
+	  if @amt.match(/\A[+-]?\d+?(\.\d+)?\Z/) == nil
+	  	@amt = 200
+	  end
+
+	  customer = Stripe::Customer.create(
+	    :email => "anonymous@mail.com",
+	    :card  => params[:stripeToken]
+	  )
+
+	  charge = Stripe::Charge.create(
+	    :customer    => customer.id,
+	    :amount      => @amt,
+	    :description => 'Bracket Customer',
+	    :currency    => 'usd'
+	  )
+
+	  rescue Stripe::CardError => e
+	  	flash[:error] = e.message
+
+	  # flash[:success] = "Thank You for your contribution!"
+	  # redirect_to '/account'
+
+	end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Sids::Application.routes.draw do
     resource :payment, only: [:new, :create]
   end
 
+  resources :donations
+
   match '/account/update_notification_prefs' => 'account#update_notification_prefs'
 
   # The priority is based upon order of creation:
